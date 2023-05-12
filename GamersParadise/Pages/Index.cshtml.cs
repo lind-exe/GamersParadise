09afe82ab6e46c67f0a5efdef32ddf7d909e62bd
Gr2dd3 <gradde23@gmail.com>
2023-05-12 15:48:29 +0200

    start on Frontend

--- GamersParadise/Pages/Index.cshtml.cs
+++ GamersParadise/Pages/Index.cshtml.cs
@@ -1,18 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GamersParadise.DAL;
+using GamersParadise.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace GamersParadise.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public List<SubCategory> SubCategories { get; set; }
 
-        public async Task OnGetAsync()
+        [BindProperty]
+        public SubCategory SubCategory { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int editid)
         {
+            SubCategories = await AdminManager.GetAllSubCategories();
+            if (editid != 0)
+            {
+                SubCategory = SubCategories.Where(x => x.Id == editid).FirstOrDefault();
 
+            }
+            return Page();
         }
-        public async Task OnPostAsync()
+        public async Task<IActionResult> OnPostAsync()
         {
-
+            return Page();
         }
     }
 }
